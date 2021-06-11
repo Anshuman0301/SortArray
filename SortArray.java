52066fc0543640752af0c0a9cdc642615d3bbf71
Anshuman bhattacharya <77188112+Anshuman0301@users.noreply.github.com>
2021-06-12 01:54:02 +0530

    Update SortArray.java

--- SortArray.java
+++ SortArray.java
@@ -11,7 +11,7 @@ public class SortArray
         int n=sc.nextInt();
 
         int arr[]=new int[n];
-        System.out.println("Enter the size of elements");
+        System.out.println("Enter the elements");
         for(int i=0;i<n;i++)
         {
             arr[i]=sc.nextInt();
